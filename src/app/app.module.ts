f471ae7b91c9d306db15a092cf183089868a15a0
hernan <hernag_09@hotmail.com>
2019-06-10 19:11:09 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { GoogleMapsComponent } from '../components/google-maps/google-maps';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    GoogleMapsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/components/google-maps/google-maps.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild } from '@angular/core';
+
+/**
+ * Generated class for the GoogleMapsComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'google-maps',
+  templateUrl: 'google-maps.html'
+})
+export class GoogleMapsComponent {
+  
+
+  @ViewChild('map') mapElement;
+  map:any
+
+  constructor() {
+   
+  }
+
+  ngOnInit(){
+    this.initMap();
+  }
+
+  initMap(){
+    let coords=new google.maps.LatLng(45,100);
+    let mapsOptions:google.maps.MapOptions={
+      center:coords,
+      zoom:14,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    }
+    this.map=new google.maps.Map(this.mapElement.nativeElement,mapsOptions)
+
+    let marker: google.maps.Marker=new google.maps.Marker({
+      map:this.map,
+      position:coords
+
+    })
+  }
+
+}
